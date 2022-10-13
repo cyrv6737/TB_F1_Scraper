69fe130b872bd40dabc905577bc866e96abd8f5e
cyrv6737 <cyrv6737@protonmail.com>
2022-10-12 22:12:33 -0400

    Implement driver lap results per driver per race

--- main.py
+++ main.py
@@ -28,11 +28,27 @@ def lap_results(year, names) :
     for race_name in names:
         temp_race = fastf1.get_session(year, race_name, 'R'); temp_race.load()
         temp_laps = temp_race.laps
-        format_lap_tables(temp_laps)
+        # format_lap_tables(temp_laps)
         csv_name = 'csv/R' + str(i + 1) + '_Lap_Results.csv'
         temp_laps.to_csv(csv_name, sep=',', encoding='utf-8', index=False) # Ensure utf-8 encoding for compat
         i = i + 1
-    
+
+def format_driver_tables(table) :
+    table.dop(labels = [], axis = 1, inplace = True)
+
+def lap_results_by_driver(year, names, drivers) :
+    i = 0
+    for race_name in names:
+        temp_race = fastf1.get_session(year, race_name, 'R'); temp_race.load()
+        temp_laps = temp_race.laps
+        for abr in drivers:
+            temp_driver = temp_laps.pick_driver(abr)
+            # format_driver_tables(temp_driver)
+            csv_name = 'csv/R' + str(i + 1) + '_Lap_' + abr + '_Results.csv'
+            temp_driver.to_csv(csv_name, sep=',', encoding='utf-8', index=False) # Ensure utf-8 encoding for compat
+        
+        i = i + 1
+
 
 # All race names for this season
 race_names = (
@@ -56,6 +72,30 @@ race_names = (
     "FORMULA 1 HONDA JAPANESE GRAND PRIX 2022"
 )
 
+drivers = (
+    "VER",
+    "RUS",
+    "LEC",
+    "HAM",
+    "PER",
+    "ALO",
+    "NOR",
+    "SAI",
+    "OCO",
+    "STR",
+    "GAS",
+    "ALB",
+    "MSC",
+    "VET",
+    "MAG",
+    "ZHO",
+    "RIC",
+    "LAT",
+    "BOT",
+    "TSU"
+)
+
 # Export CSV for all races results
 race_results(2022, race_names)
 lap_results(2022, race_names)
+# lap_results_by_driver(2022, race_names, drivers)
