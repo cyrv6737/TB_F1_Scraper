6ce767f1a764e1b223d5f2e49728b3b119944522
cyrv6737 <cyrv6737@protonmail.com>
2022-10-29 15:11:25 -0400

    Big redesign

--- main.py
+++ main.py
@@ -12,48 +12,100 @@ from pathlib import Path
 fastf1.Cache.enable_cache("cache")
 
 # create paths
-Path("csv/laps").mkdir(parents = True, exist_ok = True)
 Path("csv/results").mkdir(parents = True, exist_ok = True)
 Path("csv/tables").mkdir(parents = True, exist_ok = True)
 
-# Takes Session.results
-def format_race_table(race, race_num) :
-    race.drop(labels = ["FullName", "Abbreviation", "BroadcastName", "Q1", "Q2", "Q3", "TeamColor"], axis = 1, inplace = True)
-    race.insert(loc = 0, column = 'raceID', value = race_num)
+def get_race_results(events):
+    irace = 0
+    for race in events:
+        filename = "RaceData_" + str(race) + ".csv"
+        session = fastf1.get_session(2022, race, 'R')
+        session.load()
 
-def race_results(year, names) :
-    i = 0
-    for race_name in names:
-        temp_race = fastf1.get_session(year, race_name, 'R'); temp_race.load()
-        format_race_table(temp_race.results, (i + 1))
-        csv_name = 'csv/results/R' + str(i + 1) + '_Race_Results.csv'
-        temp_race.results.to_csv(csv_name, sep=',', encoding='utf-8', index=False) # Ensure utf-8 encoding for compat
-        i = i + 1
+        length = len(session.results.FullName)
+        results_artifact = session.results.iloc[0:length].loc[:, ['DriverNumber', 'FirstName', 'LastName', 'TeamName', 'Position', 'Time', 'Points', 'Status']]
+        # print(session.results.iloc[0:20].loc[:, ['FullName', 'TeamName', 'Position', 'Time', 'Points', 'Status']])
+        results_artifact.insert(loc = 1, column = 'raceID', value = irace)
+        df = pd.DataFrame(results_artifact)
+        df.to_csv("csv/results/" + filename, sep=',', encoding='utf-8', index=False)
+        irace = irace + 1
 
-    results = os.path.join("csv/results/", "R*.csv")
-    results = glob.glob(results)
-    df = pd.concat(map(pd.read_csv, results), ignore_index = True)
-    df.to_csv('csv/tables/results.csv', sep=',', encoding='utf-8', index=False)
+    race_table = os.path.join("csv/results/", "RaceData_*.csv")
+    race_table = glob.glob(race_table)
+    df = pd.concat(map(pd.read_csv, race_table))
+    df.to_csv("csv/tables/RaceResults.csv", sep=',', encoding='utf-8', index=False)
+
+def get_qualifier_results(events):
+    iqual = 0
+    for qualifier in events:
+        filename = "QualifierData_" + str(qualifier) + ".csv"
+        session = fastf1.get_session(2022, qualifier, 'Q')
+        session.load()
+
+        length = len(session.results.FullName)
+        results_artifact = session.results.iloc[0:length].loc[:, ['DriverNumber', 'FirstName', 'LastName', 'TeamName', 'Position', 'Q1', 'Q2', 'Q3']]
+        # print(session.results.iloc[0:20].loc[:, ['FullName', 'TeamName', 'Position', 'Time', 'Points', 'Status']])
+        results_artifact.insert(loc = 1, column = 'raceID', value = iqual)
+        df = pd.DataFrame(results_artifact)
+        df.to_csv("csv/results/" + filename, sep=',', encoding='utf-8', index=False)
+        iqual = iqual + 1
+
+    qual_table = os.path.join("csv/results/", "QualifierData_*.csv")
+    qual_table = glob.glob(qual_table)
+    df = pd.concat(map(pd.read_csv, qual_table))
+    df.to_csv("csv/tables/QualifierResults.csv", sep=',', encoding='utf-8', index=False)
 
 
-def format_lap_tables(laps, race_num) :
-    laps.drop(labels = ["LapNumber", "Sector1SessionTime", "Sector2SessionTime", "Sector3SessionTime", "SpeedI1", "SpeedI2", "SpeedFL", "SpeedST", "IsPersonalBest", "TyreLife", "FreshTyre", "Stint", "LapStartTime", "Team", "Driver", "TrackStatus", "IsAccurate", "LapStartDate" ], axis = 1, inplace = True)
-    laps.insert(loc = 0, column = 'raceID', value = race_num)
+def get_practice_results(events):
+    ifp1 = 0
+    for fp1 in events:
+        filename = "FP1_Data_" + str(fp1) + ".csv"
+        session = fastf1.get_session(2022, fp1, 'Q')
+        session.load()
 
-def lap_results(year, names) :
-    i = 0
-    for race_name in names:
-        temp_race = fastf1.get_session(year, race_name, 'R'); temp_race.load()
-        temp_laps = temp_race.laps
-        format_lap_tables(temp_laps, (i + 1))
-        csv_name = 'csv/laps/R' + str(i + 1) + '_Lap_Results.csv'
-        temp_laps.to_csv(csv_name, sep=',', encoding='utf-8', index=False) # Ensure utf-8 encoding for compat
-        i = i + 1
+        length = len(session.results.FullName)
+        results_artifact = session.results.iloc[0:length].loc[:, ['DriverNumber', 'FirstName', 'LastName', 'TeamName', 'Position', 'Time',]]
+        # print(session.results.iloc[0:20].loc[:, ['FullName', 'TeamName', 'Position', 'Time', 'Points', 'Status']])
+        results_artifact.insert(loc = 1, column = 'raceID', value = ifp1)
+        results_artifact.insert(loc = 1, column = 'Practice Session', value = '1')
+        df = pd.DataFrame(results_artifact)
+        df.to_csv("csv/results/" + filename, sep=',', encoding='utf-8', index=False)
+        ifp1 = ifp1 + 1
 
-    results = os.path.join("csv/laps/", "R*.csv")
-    results = glob.glob(results)
-    df = pd.concat(map(pd.read_csv, results), ignore_index = True)
-    df.to_csv('csv/tables/laps.csv', sep=',', encoding='utf-8', index=False)
+    ifp2 = 0
+    for fp2 in events:
+        filename = "FP2_Data_" + str(fp2) + ".csv"
+        session = fastf1.get_session(2022, fp2, 'Q')
+        session.load()
+
+        length = len(session.results.FullName)
+        results_artifact = session.results.iloc[0:length].loc[:, ['DriverNumber', 'FirstName', 'LastName', 'TeamName', 'Position', 'Time',]]
+        # print(session.results.iloc[0:20].loc[:, ['FullName', 'TeamName', 'Position', 'Time', 'Points', 'Status']])
+        results_artifact.insert(loc = 1, column = 'raceID', value = ifp2)
+        results_artifact.insert(loc = 1, column = 'Practice Session', value = '2')
+        df = pd.DataFrame(results_artifact)
+        df.to_csv("csv/results/" + filename, sep=',', encoding='utf-8', index=False)
+        ifp2 = ifp2 + 1
+
+    ifp3 = 0
+    for fp3 in events:
+        filename = "FP3_Data_" + str(fp3) + ".csv"
+        session = fastf1.get_session(2022, fp3, 'Q')
+        session.load()
+
+        length = len(session.results.FullName)
+        results_artifact = session.results.iloc[0:length].loc[:, ['DriverNumber', 'FirstName', 'LastName', 'TeamName', 'Position', 'Time',]]
+        # print(session.results.iloc[0:20].loc[:, ['FullName', 'TeamName', 'Position', 'Time', 'Points', 'Status']])
+        results_artifact.insert(loc = 1, column = 'raceID', value = ifp3)
+        results_artifact.insert(loc = 1, column = 'Practice Session', value = '3')
+        df = pd.DataFrame(results_artifact)
+        df.to_csv("csv/results/" + filename, sep=',', encoding='utf-8', index=False)
+        ifp3 = ifp3 + 1
+
+    qual_table = os.path.join("csv/results/", "FP*.csv")
+    qual_table = glob.glob(qual_table)
+    df = pd.concat(map(pd.read_csv, qual_table))
+    df.to_csv("csv/tables/FP_Results.csv", sep=',', encoding='utf-8', index=False)
 
 # All race names for this season
 race_names = (
@@ -75,31 +127,10 @@ race_names = (
     "FORMULA 1 PIRELLI GRAN PREMIO D’ITALIA 2022",
     "FORMULA 1 SINGAPORE AIRLINES SINGAPORE GRAND PRIX 2022",
     "FORMULA 1 HONDA JAPANESE GRAND PRIX 2022"
-)
-
-drivers = (
-    "VER",
-    "RUS",
-    "LEC",
-    "HAM",
-    "PER",
-    "ALO",
-    "NOR",
-    "SAI",
-    "OCO",
-    "STR",
-    "GAS",
-    "ALB",
-    "MSC",
-    "VET",
-    "MAG",
-    "ZHO",
-    "RIC",
-    "LAT",
-    "BOT",
-    "TSU"
+    "FORMULA 1 ARAMCO UNITED STATES GRAND PRIX 2022"
 )
 
 # Export CSV for all races results
-race_results(2022, race_names)
-lap_results(2022, race_names)
+get_race_results(race_names)
+get_qualifier_results(race_names)
+get_practice_results(race_names)
